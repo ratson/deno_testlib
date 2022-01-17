b0b13a410e439088ec4c152de926b6d1ee220c56
Ratson <github@ratson.name>
2022-01-17 21:25:35 +0800

    Try Path

--- mod.ts
+++ mod.ts
@@ -1,9 +1,9 @@
 import { fromFileUrl } from "https://deno.land/std@0.121.0/path/mod.ts";
-import dirname from "https://x.nest.land/denoname@0.8.2/mod.ts";
+import { Path } from "https://deno.land/x/yxz@0.10.0/path/path.ts";
 
 console.log(import.meta.url);
 console.log(new URL(".", import.meta.url));
 
-console.log(dirname(import.meta));
+console.log(Path.cache("testlib").toString());
 
 console.log(fromFileUrl(new URL(".", import.meta.url)));
