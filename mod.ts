1737eee36860757b47d491f2b08271f81493bb40
Ratson <github@ratson.name>
2022-01-17 21:00:11 +0800

    Print result of fromFileUrl

--- mod.ts
+++ mod.ts
@@ -1,1 +1,5 @@
+import { fromFileUrl } from "https://deno.land/std@0.121.0/path/mod.ts";
+
 console.log(import.meta.url);
+console.log(new URL(".", import.meta.url));
+console.log(fromFileUrl(new URL(".", import.meta.url)));
