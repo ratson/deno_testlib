445177f12d9f9775ec91938ef46a6f0b09ab7975
Ratson <github@ratson.name>
2022-01-17 21:08:09 +0800

    Try denoname@

--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
 import { fromFileUrl } from "https://deno.land/std@0.121.0/path/mod.ts";
-import dirname from "https://deno.land/x/dirname@1.1.2/mod.ts";
+import dirname from "https://x.nest.land/denoname@0.8.2/mod.ts";
 
 console.log(import.meta.url);
 console.log(new URL(".", import.meta.url));
