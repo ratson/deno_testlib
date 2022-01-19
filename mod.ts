1f5604892804e211d0df96c89806caa105cfa4ca
Ratson <github@ratson.name>
2022-01-19 21:26:30 +0800

    feat: print version

--- mod.ts
+++ mod.ts
@@ -1,8 +1,11 @@
 import { fromFileUrl } from "https://deno.land/std@0.121.0/path/mod.ts";
 import { Path } from "https://deno.land/x/yxz@0.12.0/path/path.ts";
+import version from "./version.ts";
 
 console.dir(import.meta);
 console.log(new URL(".", import.meta.url));
+
+console.log(version);
 
 console.log(Path.cache("testlib").toString());
 
