5292db39fdec8456289dc3f771ebcb55db5e6107
Ratson <github@ratson.name>
2022-01-19 21:09:14 +0800

    feat: print import.meta

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 import { fromFileUrl } from "https://deno.land/std@0.121.0/path/mod.ts";
 import { Path } from "https://deno.land/x/yxz@0.12.0/path/path.ts";
 
-console.log(import.meta.url);
+console.dir(import.meta);
 console.log(new URL(".", import.meta.url));
 
 console.log(Path.cache("testlib").toString());
