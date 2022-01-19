1cba383d32c27d4bde00f82ae8aa0bf13034faa3
Ratson <github@ratson.name>
2022-01-19 21:07:48 +0800

    chore: update yxz

--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
 import { fromFileUrl } from "https://deno.land/std@0.121.0/path/mod.ts";
-import { Path } from "https://deno.land/x/yxz@0.10.0/path/path.ts";
+import { Path } from "https://deno.land/x/yxz@0.12.0/path/path.ts";
 
 console.log(import.meta.url);
 console.log(new URL(".", import.meta.url));
