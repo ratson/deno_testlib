abb1311fd9348258d9926eb278b69629368d5202
Ratson <github@ratson.name>
2022-01-19 22:34:08 +0800

    fix: return version

--- version.ts
+++ version.ts
@@ -3,7 +3,7 @@ import meta from "./meta.ts";
 function getVersion(s: string) {
   const m = s.match(/@((\d+\.)?(\d+\.)?(\d+))/);
   if (!m) return;
-  console.log(m[1]);
+  return m[1];
 }
 
 export default getVersion(meta.url);
