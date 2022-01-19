68ff6657f6840bf4fab12c7d9a793565b4b7c8f9
Ratson <github@ratson.name>
2022-01-19 21:54:49 +0800

    ci(github): use codfish/semantic-release-action

--- .github/release.config.js
+++ .github/release.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  "branches": [
-    { name: "main" },
-    { name: "refs/tags/*", range: "x.x.x" },
-  ],
   "plugins": [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
